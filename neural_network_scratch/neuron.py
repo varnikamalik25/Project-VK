0d3d5cf17089cd60606e610524df300e88af1a80
Kartikeya Srivastav <kartikeya251@gmail.com>
2026-08-04 10:37:15 +0530

    Activation Functions

--- neural_network_scratch/neuron.py
+++ neural_network_scratch/neuron.py
@@ -7,18 +7,28 @@ class Layer:
         self.weights = 0.10*np.random.randn(inputs,neurons)
         self.biases = np.zeros((1,neurons))
 
+
+    def take_exponent(self,inputs):
+        exp_values = np.sin(inputs-np.max(inputs,axis=1,keepdims=True))
+        self.outputs = exp_values/np.sum(exp_values,axis=1,keepdims=True)
+
+
+
+
     def forward(self,inputs):
         self.outputs = np.dot(inputs,self.weights) + self.biases
 
 
 
-X = [[1,2,3,2.5],
-[2.0,5.0,-1.0,2.0],
-[-1.5,2.7,3.3,-0.8]]
+X = [[1,2,3],
+[2.0,5.0,-1.0],
+[-1.5,2.7,3.3]]
 
-layer1 = Layer(4,5)
-layer2 = Layer(5,2)
+layer1 = Layer(3,3)
+layer2 = Layer(3,3)
 
-layer1.forward(X)
+layer1.take_exponent(X)
+#print(layer1.outputs)
 layer2.forward(layer1.outputs)
 print(layer2.outputs)
+
